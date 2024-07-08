a8f1943a85c4bd9c0d7b46562d17dea079894285
IRMEDEMIR <demirirem.0202@gmail.com>
2024-07-08 15:02:40 +0300

    Sahalara tablo eklendi.

--- ParsingProjectMVC/Controllers/SahalarController.cs
+++ ParsingProjectMVC/Controllers/SahalarController.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ParsingProjectMVC.Models;
 
 namespace ParsingProjectMVC.Controllers
 {
     public class SahalarController : Controller
     {
-        public IActionResult Index()
+        private List<Saha> sahalar = new List<Saha>
         {
-            return View();
+            // Örnek veriler
+            new Saha { Id = 1, Name = "Saha 1" },
+            new Saha { Id = 2, Name = "Saha 2" },
+            new Saha { Id = 3, Name = "Saha 3" },
+            // Daha fazla örnek veri ekleyin...
+        };
+
+        public IActionResult Index(int pageNumber = 1, int pageSize = 10)
+        {
+            var pagedSahalar = sahalar
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            var totalItems = sahalar.Count;
+
+            ViewBag.PageNumber = pageNumber;
+            ViewBag.PageSize = pageSize;
+            ViewBag.TotalItems = totalItems;
+
+            return View(pagedSahalar);
         }
     }
 }
+
---
+++ ParsingProjectMVC/Models/Saha.cs
@@ -0,0 +1,8 @@
+﻿namespace ParsingProjectMVC.Models
+{
+    public class Saha
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

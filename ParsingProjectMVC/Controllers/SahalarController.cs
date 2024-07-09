1a9e0fe137c040382f7e455c2884937056baa620
IRMEDEMIR <demirirem.0202@gmail.com>
2024-07-09 16:46:12 +0300

    Commit

--- ParsingProjectMVC/Controllers/SahalarController.cs
+++ ParsingProjectMVC/Controllers/SahalarController.cs
@@ -5,7 +5,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
-
+//tekrar
 namespace ParsingProjectMVC.Controllers
 {
     public class SahalarController : Controller

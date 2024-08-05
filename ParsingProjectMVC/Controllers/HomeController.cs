b7399a864c73101387595cedc01cfa1b43ba8fa2
ayskavak <134221095+ayskavak@users.noreply.github.com>
2024-08-05 10:28:20 +0300

    .

--- ParsingProjectMVC/Controllers/HomeController.cs
+++ ParsingProjectMVC/Controllers/HomeController.cs
@@ -42,6 +42,12 @@ namespace ParsingProjectMVC.Controllers
             return View();
         }
 
+
+        public IActionResult SurfaceDetail()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
